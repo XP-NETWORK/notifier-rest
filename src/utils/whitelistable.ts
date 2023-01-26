b9f6490d8bbd01f62cac07d8c3643f4d87221c68
MuhammadFassihHaider <fassih.haider786@gmail.com>
2023-01-26 15:40:08 +0000

    updated function for whitelistable check

--- src/utils/whitelistable.ts
+++ src/utils/whitelistable.ts
@@ -1,135 +1,142 @@
 import axios from 'axios';
 
-const checkFunctions = [
-  'safeTransferFrom',
-  '_isApprovedOrOwner',
-  'isApprovedForAll',
-  'getApproved',
-  '_requireMinted',
-  '_safeTransfer',
-  '_transfer',
-  '_beforeTokenTransfer',
-  '_afterTokenTransfer',
-];
+/**
+ * function keys to ignore eg : `toString` function if exists in solidity code
+ */
+const keysToNotCheck = ['toString'];
+/**
+ * Add function body without spaces here which we want to allow
+ * Empty string is to ignore interface
+ */
+const checkFunctionsAndDefinitioins = {
+  safeTransferFrom: [
+    '',
+    '{require(_isApprovedOrOwner(_msgSender(),tokenId),"ERC721:transfercallerisnotownernorapproved");_safeTransfer(from,to,tokenId,_data);}',
+  ],
+  _isApprovedOrOwner: [
+    '',
+    '{require(_exists(tokenId),"ERC721:operatorqueryfornonexistenttoken");addressowner=ERC721.ownerOf(tokenId);return(spender==owner||getApproved(tokenId)==spender||ERC721.isApprovedForAll(owner,spender));}',
+  ],
+  isApprovedForAll: ['', '{return_operatorApprovals[owner][operator];}'],
+  getApproved: [
+    '',
+    '{_requireMinted(tokenId);return_tokenApprovals[tokenId];}',
+    '{require(_exists(tokenId),"ERC721:approvedqueryfornonexistenttoken");return_tokenApprovals[tokenId];}',
+  ],
+  _requireMinted: [''],
+  _safeTransfer: [
+    '',
+    '{_transfer(from,to,tokenId);require(_checkOnERC721Received(from,to,tokenId,_data),"ERC721:transfertononERC721Receiverimplementer");}',
+  ],
+  _transfer: [
+    '',
+    '{require(ERC721.ownerOf(tokenId)==from,"ERC721:transferoftokenthatisnotown");require(to!=address(0),"ERC721:transfertothezeroaddress");_beforeTokenTransfer(from,to,tokenId);_approve(address(0),tokenId);_balances[from]-=1;_balances[to]+=1;_owners[tokenId]=to;emitTransfer(from,to,tokenId);}',
+  ],
+  _beforeTokenTransfer: ['', '{}'],
+  _afterTokenTransfer: [''],
+};
+/**
+ * Function to extract body of function
+ * @param str function that we want to extract body of
+ * @returns function body
+ */
+function extractFunctionBody(str: string) {
+  let stack = [];
+  let start = -1;
+  let end = -1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '{') {
+      if (stack.length === 0) {
+        start = i;
+      }
+      stack.push(str[i]);
+    } else if (str[i] === '}') {
+      stack.pop();
+      if (stack.length === 0) {
+        end = i;
+        break;
+      }
+    }
+  }
+  return str.slice(start, end + 1);
+}
 
-const allowedFunctionsRepetead = [
-  'safeTransferFrom(from,to,tokenId,"");',
-  'asd(from,to,tokenId,"");',
-];
+/**
+ * Function to check if contract is whitelistable or not
+ * @param explorerApi explorer api to use for
+ * @param contractAddress the contract address to check
+ * @param apiKey api key of explorer api
+ * @returns A boolean, true if all test cases passes false if none pass
+ */
 
 export const isWhitelistable = async (
   explorerApi: string,
   contractAddress: string,
   apiKey: string
 ) => {
-  if (!explorerApi || !contractAddress || !apiKey) return false;
+  const data = await axios.get(
+    `${explorerApi}/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${apiKey}`
+  );
 
-  const endpoint = `${explorerApi}/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${apiKey}`;
-  console.log({ endpoint });
+  /**
+   * If not verified return false
+   */
 
-  try {
-    const data = await axios.get(endpoint);
-    // if(data.status !== 200) return false;
-    let isNotVerified = false;
+  if (data.status !== 200) return false;
 
-    let sourceCode = data.data.result[0].SourceCode;
-    let contractName = data.data.result[0].ContractName;
-    console.log('Contract Name = ', contractName);
+  const doubleSlashCommentsRegex = /\/\/.*?(\\n)/g;
+  const nextLineRegex = /\\n/g;
+  const escapeSlashRegex = /\\"/g;
+  const blockCommentsRegex = /\/\*[\s\S]*?\*\//g;
+  const functionsRegex = /(function)\s+(\w+)\s*\([^]*?\{([^]*?)\}/gms;
+  const functionNamesRegex = /(function|constructor)\s+(\w+)\s*\(/;
+  const spaceRegex = /\s/g;
 
-    if (!sourceCode.trim()) {
-      console.log(isNotVerified);
-      return isNotVerified;
-    }
+  let sourceCode = data.data.result[0].SourceCode;
+  let contractName = data.data.result[0].ContractName;
+  console.log('Contract Name = ', contractName);
+  sourceCode = sourceCode
+    .replace(doubleSlashCommentsRegex, '')
+    .replace(nextLineRegex, '')
+    .replace(escapeSlashRegex, '"')
+    .replace(blockCommentsRegex, '');
 
-    let sources: any;
-    let functions: string[] = [];
-    let fileCodes: string[] = [];
+  /**
+   * if is upgradeable, return false
+   */
 
-    if (sourceCode.substring(0, 2) === '{{') {
-      sourceCode = sourceCode.slice(1, sourceCode.length - 1);
-      sources = JSON.parse(sourceCode).sources;
+  if (sourceCode.includes('Upgradeable')) return false;
 
-      Object.keys(sources).forEach((key) => {
-        if (!key.includes('@openzeppelin')) {
-          let func = sources[key].content.split('function ');
+  const matches = sourceCode.match(functionsRegex);
 
-          fileCodes.push(
-            sources[key].content
-              .replaceAll(/(\r\n|\n|\r)/gm, '')
-              .replaceAll(' ', '')
-          );
+  let functions: Record<string, string> = {};
 
-          func.forEach((e: string, index: number) => {
-            if (index > 0) {
-              let str = e.replace(/\n/g, '');
-              str = str.slice(0, str.indexOf('{'));
-              functions.push(str);
-            }
-          });
-        } else {
-          if (key.includes('ERC721Upgradeable.sol')) {
-            isNotVerified = true;
-          }
-        }
-      });
-    } else {
-      sources = sourceCode;
-      let fromIndex = sources.indexOf(`contract ${contractName}`);
-      let mainContractFile = sources.substring(fromIndex);
-
-      let isIndex = mainContractFile.indexOf('is') + 2;
-      let leftCurlyBracketIndex = mainContractFile.indexOf('{');
-      let checkInheritance = mainContractFile
-        .substring(isIndex, leftCurlyBracketIndex)
-        .replaceAll(' ', '')
-        .split(',');
-
-      console.log('Inheritance: ', checkInheritance);
-
-      if (checkInheritance.indexOf('ERC721Upgradeable') > -1)
-        return isNotVerified;
-
-      let func = mainContractFile.split('function ');
-      fileCodes.push(
-        mainContractFile.replaceAll(/(\r\n|\n|\r)/gm, '').replaceAll(' ', '')
-      );
-      func.forEach((e: string, index: number) => {
-        if (index > 0) {
-          let str = e.replace(/\n/g, '');
-          str = str.slice(0, str.indexOf('{'));
-          functions.push(str);
-        }
-      });
-    }
-    let isSafeTransfer = false;
-    if (!isNotVerified) {
-      functions.forEach((func: any) => {
-        if (func.includes('safeTransferFrom')) {
-          isSafeTransfer = true;
-        }
-        isNotVerified = func.match(new RegExp(checkFunctions.join('|'), 'g'))
-          ? true
-          : isNotVerified;
-      });
-    }
-
-    console.log('first condition', isNotVerified);
-
-    if (isNotVerified && isSafeTransfer) {
-      fileCodes.forEach((code: any) => {
-        for (const allow of allowedFunctionsRepetead) {
-          isNotVerified = code.includes(allow) ? false : isNotVerified;
-          if (!isNotVerified) {
-            break;
-          }
-        }
-      });
-    }
-
-    console.log(!isNotVerified);
-    return !isNotVerified;
-  } catch (ex) {
-    console.log('ERROR', ex);
-    return false;
+  if (matches) {
+    matches.forEach((match: string) => {
+      const funcName = match.match(functionNamesRegex)![2];
+      const funcBody = extractFunctionBody(match).replace(spaceRegex, '');
+      functions[funcName] = funcBody;
+    });
   }
+
+  let isVerified = true;
+
+  for (let [functionName, functionBody] of Object.entries(functions)) {
+    if (
+      //@ts-ignore
+      checkFunctionsAndDefinitioins[functionName] &&
+      !keysToNotCheck.includes(functionName)
+    ) {
+      console.log(functionName, functionBody);
+      if (
+        //@ts-ignore
+        checkFunctionsAndDefinitioins[functionName].indexOf(functionBody) == -1
+      ) {
+        isVerified = false;
+        break;
+      }
+    }
+  }
+
+  return isVerified;
 };
