c607c17bd0a25bbcf8978012f9a78a99e6f2c04c
alexadoniev <Alexeya@xp.network>
2023-11-01 09:35:11 +0000

    add SOCKET_PATH env to config

--- src/socket.ts
+++ src/socket.ts
@@ -6,12 +6,15 @@ import { ClientEvents, ServerEvents } from './events';
 export function socketMain(
   server: http.Server
 ): Server<ClientEvents, ServerEvents> {
+  const socketPath: string | undefined = process.env.SOCKET_PATH;
+
   const opts = {
     cors: {
       origin: '*',
     },
+    ...(socketPath ? { path: socketPath } : {}),
   };
-
+  console.log(opts);
   const io = new Server<ClientEvents, ServerEvents>(server, opts);
 
   return io;
